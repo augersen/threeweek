9447e15bb1b2fc724566ff849da6a4244ee624b0
s236369 <s236369@dtu.dk>
2025-01-08 15:09:30 +0100

    whoopsie :3

--- main/Main.java
+++ main/Main.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args){
         //startGame();
-        //test
+
         //Startmenu
         Application.launch(StartMenu.class, args);
     }
