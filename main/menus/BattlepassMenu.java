4fd7775d6100781319b685ee9ad84095ea583c18
Lasse Rewers <lasse@rewers.dk>
2025-01-17 23:20:13 +0100

    Reducering af min voldsomme redundans

--- main/menus/BattlepassMenu.java
+++ main/menus/BattlepassMenu.java
@@ -50,26 +50,11 @@ public class BattlepassMenu extends Application {
 
         // Actions for each award
         Map<Integer, Runnable> rewardActions = new HashMap<>();
-        rewardActions.put(1, this::unlockFeature1);
-        rewardActions.put(2, this::unlockFeature2);
-        rewardActions.put(3, this::unlockFeature3);
-        rewardActions.put(4, this::unlockFeature4);
-        rewardActions.put(5, this::unlockFeature5);
-        rewardActions.put(6, this::unlockFeature6);
-        rewardActions.put(7, this::unlockFeature7);
-        rewardActions.put(8, this::unlockFeature8);
-        rewardActions.put(9, this::unlockFeature9);
-        rewardActions.put(10, this::unlockFeature10);
-        rewardActions.put(11, this::unlockFeature11);
-        rewardActions.put(12, this::unlockFeature12);
-        rewardActions.put(13, this::unlockFeature13);
-        rewardActions.put(14, this::unlockFeature14);
-        rewardActions.put(15, this::unlockFeature15);
-        rewardActions.put(16, this::unlockFeature16);
-        rewardActions.put(17, this::unlockFeature17);
-        rewardActions.put(18, this::unlockFeature18);
-        rewardActions.put(19, this::unlockFeature19);
-        rewardActions.put(20, this::unlockFeature20);
+        for (int i = 1; i <= rewardNames.length; i++) {
+            int finalI = i; // Needs to be final for use of i lambda
+            rewardActions.put(i, () -> unlockFeature(finalI, rewardNames[finalI - 1]));
+}
+
 
         // Awards row
         HBox awardsRow = new HBox(20);
@@ -160,123 +145,9 @@ public class BattlepassMenu extends Application {
         fadeOut.play();
     }
 
-    private void unlockFeature1() {
-        System.out.println("award 1");
-        showPopUp("Congratulations on unlocking Musk!");
-        battlepassConfig = "Battlepass1";
-    }
-
-    private void unlockFeature2() {
-        System.out.println("award 2");
-        showPopUp("Congratulations on unlocking Lion!");
-        battlepassConfig = "Battlepass2";
-    }
-
-    private void unlockFeature3() {
-        System.out.println("award 3");
-        showPopUp("Congratulations on unlocking award3!");
-        battlepassConfig = "Battlepass3";
-    }
-
-    private void unlockFeature4() {
-        System.out.println("award 4");
-        showPopUp("Congratulations on unlocking award4!");
-        battlepassConfig = "Battlepass4";
-    }
-
-    private void unlockFeature5() {
-        System.out.println("award 5");
-        showPopUp("Congratulations on unlocking award5!");
-        battlepassConfig = "Battlepass5";
-    }
-
-    private void unlockFeature6() {
-        System.out.println("award 6");
-        showPopUp("Congratulations on unlocking award6!");
-        battlepassConfig = "Battlepass6";
-    }
-
-    private void unlockFeature7() {
-        System.out.println("award 7");
-        showPopUp("Congratulations on unlocking award7!");
-        battlepassConfig = "Battlepass7";
-    }
-
-    private void unlockFeature8() {
-        System.out.println("award 8");
-        showPopUp("Congratulations on unlocking award8!");
-        battlepassConfig = "Battlepass8";
-    }
-
-    private void unlockFeature9() {
-        System.out.println("award 9");
-        showPopUp("Congratulations on unlocking award9!");
-        battlepassConfig = "Battlepass9";
-    }
-
-    private void unlockFeature10() {
-        System.out.println("award 10");
-        showPopUp("Congratulations on unlocking award10!");
-        battlepassConfig = "Battlepass10";
-    }
-
-    private void unlockFeature11() {
-        System.out.println("award 11");
-        showPopUp("Congratulations on unlocking award11!");
-        battlepassConfig = "Battlepass11";
-    }
-
-    private void unlockFeature12() {
-        System.out.println("award 12");
-        showPopUp("Congratulations on unlocking award12!");
-        battlepassConfig = "Battlepass12";
-    }
-
-    private void unlockFeature13() {
-        System.out.println("award 13");
-        showPopUp("Congratulations on unlocking award13!");
-        battlepassConfig = "Battlepass13";
-    }
-
-    private void unlockFeature14() {
-        System.out.println("award 14");
-        showPopUp("Congratulations on unlocking award14!");
-        battlepassConfig = "Battlepass14";
-    }
-
-    private void unlockFeature15() {
-        System.out.println("award 15");
-        showPopUp("Congratulations on unlocking award15!");
-        battlepassConfig = "Battlepass15";
-    }
-
-    private void unlockFeature16() {
-        System.out.println("award 16");
-        showPopUp("Congratulations on unlocking award16!");
-        battlepassConfig = "Battlepass16";
-    }
-
-    private void unlockFeature17() {
-        System.out.println("award 17");
-        showPopUp("Congratulations on unlocking award17!");
-        battlepassConfig = "Battlepass17";
-    }
-
-    private void unlockFeature18() {
-        System.out.println("award 18");
-        showPopUp("Congratulations on unlocking award18!");
-        battlepassConfig = "Battlepass18";
-    }
-
-    private void unlockFeature19() {
-        System.out.println("award 19");
-        showPopUp("Congratulations on unlocking award19!");
-        battlepassConfig = "Battlepass19";
-    }
-
-    private void unlockFeature20() {
-        System.out.println("award 20");
-        showPopUp("Congratulations on unlocking award20!");
-        battlepassConfig = "Battlepass20";
-    }
+    private void unlockFeature(int awardId, String awardName) {
+        System.out.println("award " + awardId);
+        showPopUp("Congratulations on unlocking " + awardName + "!");
+        battlepassConfig = "Battlepass" + awardId;
+    }    
 }
